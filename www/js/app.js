9780f218a8a3b8fa8811bc48d4df02463cb81b7e
crosalot <crosalot#gmail.com>
2015-07-08 13:56:14 -0300

    add sign in before create report

--- www/js/app.js
+++ www/js/app.js
@@ -80,5 +80,5 @@ angular.module('epihack', [
         }
     });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/app/report');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,43 +2,6 @@ angular.module('epihack.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-    // With the new view caching in Ionic, Controllers are only called
-    // when they are recreated or on app start, instead of every page change.
-    // To listen for when this page is active (for example, to refresh data),
-    // listen for the $ionicView.enter event:
-    //$scope.$on('$ionicView.enter', function(e) {
-    //});
-
-    // Form data for the login modal
-    $scope.loginData = {};
-
-    // Create the login modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-        scope: $scope
-    }).then(function(modal) {
-        $scope.modal = modal;
-    });
-
-    // Triggered in the login modal to close it
-    $scope.closeLogin = function() {
-        $scope.modal.hide();
-    };
-
-    // Open the login modal
-    $scope.login = function() {
-        $scope.modal.show();
-    };
-
-    // Perform the login action when the user submits the login form
-    $scope.doLogin = function() {
-        console.log('Doing login', $scope.loginData);
-
-        // Simulate a login delay. Remove this and replace with your login
-        // code if using a login system
-        $timeout(function() {
-            $scope.closeLogin();
-        }, 1000);
-    };
 })
 
 .controller('PlaylistsCtrl', function($scope) {
@@ -55,9 +18,71 @@ angular.module('epihack.controllers', [])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
-.controller('ReportCtrl', function($scope) {
+.controller('ReportCtrl', function($scope, $ionicModal, $timeout) {
+
+    // =============
+    // USER
+    // =============
+    $scope.user = {
+        gender: '',
+        yearOfBirth: '',
+        origin: ''
+    };
+    $scope.genders = GENDERS;
+    $scope.countries = COUNTRIES;
+    $scope.years = YEAR_OF_BIRTH;
+
+    // For force logout
+    //window.localStorage.removeItem('user');
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+
+        if (window.localStorage['user']) {
+            $scope.user = JSON.parse(window.localStorage['user']);
+        }
+        else {
+            $scope.login();
+        }
+
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function() {
+        $scope.modal.show();
+    };
+
+
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+        // TODO: POST API to epihack
+
+        window.localStorage['user'] = JSON.stringify($scope.user);
+        console.log('Doing login', $scope.user);
+
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+            $scope.closeLogin();
+        }, 1000);
+    };
+
+    // =============
+    // REPORT
+    // =============
     $scope.report = {};
-    $scope.doReport = function (report) {
+    $scope.doReport = function (form) {
+
+        // TODO: POST API to epihack
         console.log($scope.report);
     };
 });
